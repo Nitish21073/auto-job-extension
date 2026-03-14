18a1758453100fd291d24dd26b5ccec7c005ac52
Nitish21073 <nitish21073@gmail.com>
2026-03-14 22:33:15 +0530

    feat(ui): add interface to diaplay saved jobs

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,28 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <>hi</>;
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    chrome.storage.local.get(["jobs"], (data) => {
+      setJobs(data.jobs || []);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2>Saved Jobs</h2>
+
+      {jobs.map((job, index) => (
+        <div key={index}>
+          <h4>{job.role}</h4>
+
+          <p>{job.company}</p>
+
+          <p>{job.location}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 export default App;
